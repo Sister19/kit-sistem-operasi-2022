719a8aa8999d4e12e78662d3cbe8dff25ec2e8b1
Lock1 <30568743+Lock1@users.noreply.github.com>
2022-04-01 23:36:28 +0700

    Penggantian ukuran default kernel
    Penggantian ukuran default kernel dari 16 ke 15, sesuai spesifikasi

--- milestone-1/src/asm/bootloader.asm
+++ milestone-1/src/asm/bootloader.asm
@@ -6,7 +6,7 @@
 
 	bits 16
 KSEG	equ	0x1000		;Lokasi kernel = 0x10000
-KSIZE	equ	16		;Ukuran kernel = 16 sektor
+KSIZE	equ	15		;Ukuran kernel = 15 sektor
 KSTART	equ	1		;Lokasi kernel = sektor 1
 
 	;boot loader starts at 0 in segment 0x7c00
